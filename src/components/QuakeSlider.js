51e8e05b5b3a8d0da8518c8abac22ca3c6a74f35
pplflymoon <pplflymoon2013@gmail.com>
2017-05-19 21:39:17 +1200

    #slider to be continued

---
+++ src/components/QuakeSlider.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var React = require('react');
+var ReactNative = require('react-native');
+var {
+    Slider,
+    Text,
+    StyleSheet,
+    View,
+} = ReactNative;
+
+export default  class QuakeSlider extends React.Component {
+    static defaultProps = {
+        value: 0,
+    };
+
+    state = {
+        value: this.props.value,
+    };
+
+    render() {
+        return (
+            <View>
+                <Text style={styles.text} >
+                    {this.state.value && +this.state.value.toFixed(3)}
+                </Text>
+                <Slider
+                    {...this.props}
+                    onValueChange={(value) => this.setState({value: value})} />
+            </View>
+        );
+    }
+}
+
+
+
+var styles = StyleSheet.create({
+    slider: {
+        height: 10,
+        margin: 10,
+    },
+    text: {
+        fontSize: 14,
+        textAlign: 'center',
+        fontWeight: '500',
+        margin: 10,
+    },
+});
+
+
+
