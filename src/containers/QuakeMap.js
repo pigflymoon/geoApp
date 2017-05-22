b21ff1a705ccb53aa4090f302121cb1876be8ecd
pigflymoon <pplflymoon2015@gmail.com>
2017-05-22 22:55:49 +1200

    #slider to be continued

--- src/containers/QuakeMap.js
+++ src/containers/QuakeMap.js
@@ -161,7 +161,6 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        // alignItems: 'center',
         backgroundColor: '#F5FCFB',
     },
     map: {
