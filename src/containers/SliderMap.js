57bcca67747b5a4a1d80c71b20e88dae1add7c6c
pigflymoon <pplflymoon2015@gmail.com>
2017-05-23 12:33:01 +1200

    #Change slider map change #map has to be cleaned before each change

--- src/containers/SliderMap.js
+++ src/containers/SliderMap.js
@@ -7,8 +7,9 @@ import {
 } from 'react-native';
 import {bind} from '../utils/utils';
 
-import MapView from 'react-native-maps';
-import QuakeSlider from '../components/QuakeSlider'
+// import MapView from 'react-native-maps';
+import QuakeSlider from '../components/QuakeSlider';
+import QuakeMap from '../components/QuakeMap';
 
 import axios from 'axios';
 
@@ -29,22 +30,13 @@ export default class SliderMap extends Component {
         super(props);
 
         this.state = {
-            level: LEVEL,
-            region: {
-                latitude: LATITUDE,
-                longitude: LONGITUDE,
-                latitudeDelta: LATITUDE_DELTA,
-                longitudeDelta: LONGITUDE_DELTA,
-            },
-            markers: [],
-            loading: true,
-            error: null
+            level: LEVEL
         };
         bind(this)('handleChooseLevel')
     }
 
     handleChooseLevel(stat) {
-        console.log('stat ',stat)
+        console.log('stat ', stat)
         if (stat <= 3) {
             this.setState({level: 3})
         } else if (stat <= 4) {
@@ -59,103 +51,27 @@ export default class SliderMap extends Component {
 
     }
 
-    componentDidMount() {
-        let self = this
-        let url = nps_url + this.state.level;
-        // console.log('level ',this.state.level)
-        axios.get(url)
-            .then(function (result) {
-                console.log('url',url)
-                for (let post of result.data.features) {
-                    let time = post.properties.time;
-                    var time = new Date(time);
-                    time = time.toString().split('GMT')[0];
-                    var marker = {
-                        locality: post.properties.locality,
-                        time: time,
-                        depth: post.properties.depth.toFixed(1) + ' km',
-                        magnitude: post.properties.magnitude.toFixed(1),
-                        coordinates: {
-                            longitude: post.geometry.coordinates[0],
-                            latitude: post.geometry.coordinates[1]
-                        }
-
-                    };
-                    markers.push(marker);
-                } // for
-
-                // markers['coordinates'] = coordinates;
-                console.log('markers', markers)
-            }); //then
-
-        this.setState({
-                markers: markers,
-                loading: false,
-                error: null
-            }
-        );
-
-
-    }
-
-    renderPosts() {
-        if (this.state.error) {
-            return this.renderError();
-        }
-
-        return (
-            <MapView
-                style={styles.map}
-                initialRegion={{
-                    latitude: LATITUDE,
-                    longitude: LONGITUDE,
-                    latitudeDelta: LATITUDE_DELTA,
-                    longitudeDelta: LONGITUDE_DELTA,
-                }}>
-                {this.state.markers.map((marker, index) => (
-                    <MapView.Marker
-                        coordinate={marker.coordinates}
-                        title={marker.locality}
-                        description={`Time: ${marker.time}  Depth: ${marker.depth}  Magnitude: ${marker.magnitude}`}
-                        key={index}
-                    >
-                    </MapView.Marker>
-                ))}
-
-
-            </MapView>
-        );
-    }
-
-    renderLoading() {
-        return <Text>Loading...</Text>;
-    }
-
-    renderError() {
-        return (
-            <Text>
-                Uh oh: {this.state.error.message}
-            </Text>
-        );
-    }
-
     render() {
         return (
             <View style={styles.container}>
-
-
                 <Text>Map</Text>
-
-                {this.state.loading ?
-                    this.renderLoading()
-                    : this.renderPosts()}
+                <QuakeMap type="SliderMap"
+                          nps_source={nps_url} level={this.state.level}
+                          latitude={LATITUDE}
+                          longitude={LONGITUDE}
+                          latitudeDelta={LATITUDE_DELTA}
+                          longitudeDelta={LONGITUDE_DELTA}
+                          screenWidth={SCREEN_WIDTH}
+                />
 
                 <View>
 
                     <QuakeSlider style={styles.label}
                                  onChooseLevel={this.handleChooseLevel}
                                  minimumValue={1}
-                                 maximumValue={12}/>
+                                 maximumValue={12}
+                                 step={1}
+                    />
                 </View>
             </View>
         )
