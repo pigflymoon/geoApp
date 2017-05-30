77d23825304b150fa71b6b489a5e752323f7e332
pigflymoon <pplflymoon2015@gmail.com>
2017-05-30 20:46:52 +1200

    :white_check_mark: news list

--- app/screens/News.js
+++ app/screens/News.js
@@ -2,21 +2,79 @@ import React, {Component} from 'react';
 import {
     Text,
     View,
-    ScrollView
+    ScrollView,
+    Linking
 } from 'react-native';
 import {List, ListItem} from 'react-native-elements';
+import axios from 'axios';
+import {bind} from '../utils/utils';
+
+var news;
 
 export default class News extends Component {
-    render(){
-        return(
+
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            dataSource: [],
+            isLoading: true
+        };
+        bind(this)('renderLoadingView','goToURL')
+    }
+
+    componentDidMount() {
+        axios.get(`https://api.geonet.org.nz/news/geonet`)
+            .then(res => {
+                news = res.data.feed.map(function (item) {
+                    if (item.published) {
+                        item.published = item.published.slice(0, 10).replace(/-/g, "-")
+                    }
+
+                    return item;
+                });
+                this.setState({
+                    dataSource: news,
+                    isLoading: false
+                })
+            });
+
+
+    }
+
+    renderLoadingView() {
+        return (
+            <ScrollView>
+                <Text>Loading...</Text>
+            </ScrollView>
+        )
+    }
+
+    goToURL(url) {
+        Linking.canOpenURL(url).then(supported => {
+            if (supported) {
+                Linking.openURL(url);
+            } else {
+                console.log('Don\'t know how to open URI: ' + url);
+            }
+        });
+    }
+
+    render() {
+        if (this.state.isLoading) {
+            return this.renderLoadingView();
+        }
+
+        return (
             <ScrollView>
                 <List>
-                    <ListItem
-                        key={'1'}
-
-                        title='test'
-
-                    />
+                    {this.state.dataSource.map((news, index) => (
+                        <ListItem
+                            key={index}
+                            title={news.title}
+                            subtitle={news.published}
+                            onPress={() => this.goToURL(news.link)}
+                        />
+                    ))}
                 </List>
             </ScrollView>
         )
