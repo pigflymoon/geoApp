f84ab331fd1cb4e000ca4b90213987b4c22caae6
pigflymoon <pplflymoon2015@gmail.com>
2017-06-08 14:39:37 +1200

    :white_check_mark: Set every half hour call api data and page content will re-render automatically

--- app/screens/QuakesList.js
+++ app/screens/QuakesList.js
@@ -7,6 +7,7 @@ import {
     Icon,
     StyleSheet
 } from 'react-native';
+
 import {List, ListItem} from 'react-native-elements';
 import axios from 'axios';
 import {bind} from '../utils/utils';
@@ -26,36 +27,43 @@ export default class QuakesList extends Component {
 
     componentDidMount() {
         // QuakesApi.getAllQuakes()
-        axios.get(`https://api.geonet.org.nz/quake?MMI=0`)
-            .then(res => {
-                const filterData = [];
-                quakes = res.data.features.reduce((array, value) => {
-                    // if condition is our filter
-                    if (value.properties.mmi >= 2) {
-                        // what happens inside the filter is the map
-                        let time = value.properties.time;
-                        var utime = new Date(time);
-                        utime = new Date(utime.toUTCString().slice(0, -4));
-                        utime = utime.toString().split('GMT')[0];
 
-                        time = new Date(time);
-                        time = time.toString().split('GMT')[0];
+        this.timer = setInterval(() => {
+            console.log('I do not leak!');
 
-                        value.utime = utime;
-                        value.properties.time = time;
-                        value.properties.magnitude = value.properties.magnitude.toFixed(1);
-                        value.properties.depth = value.properties.depth.toFixed(1) + ' km';
+            axios.get(`https://api.geonet.org.nz/quake?MMI=0`)
+                .then(res => {
+                    const filterData = [];
+                    quakes = res.data.features.reduce((array, value) => {
+                        // if condition is our filter
+                        if (value.properties.mmi >= 2) {
+                            // what happens inside the filter is the map
+                            let time = value.properties.time;
+                            var utime = new Date(time);
+                            utime = new Date(utime.toUTCString().slice(0, -4));
+                            utime = utime.toString().split('GMT')[0];
 
-                        array.push(value);
-                    }
-                    return array.slice(0, 10);
-                }, filterData)
+                            time = new Date(time);
+                            time = time.toString().split('GMT')[0];
 
-                this.setState({
-                    dataSource: quakes,
-                    isLoading: false
+                            value.utime = utime;
+                            value.properties.time = time;
+                            value.properties.magnitude = value.properties.magnitude.toFixed(1);
+                            value.properties.depth = value.properties.depth.toFixed(1) + ' km';
+
+                            array.push(value);
+                        }
+                        return array.slice(0, 10);
+                    }, filterData)
+
+                    this.setState({
+                        dataSource: quakes,
+                        isLoading: false
+                    })
                 })
-            })
+        }, 1000 * 60 * 60 * 0.5);
+
+
 
     }
 
