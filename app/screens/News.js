13344f7d09ad6b982fcbc564ed480442c7af0d7c
pigflymoon <pplflymoon2015@gmail.com>
2017-06-12 08:21:35 +1200

    :white_check_mark: Notification

--- app/screens/News.js
+++ app/screens/News.js
@@ -3,10 +3,17 @@ import {
     Text,
     View,
     ScrollView,
-    Linking
+    StyleSheet,
+    Linking,
+    AppState,
+    Picker,
+    Platform
 } from 'react-native';
 import {List, ListItem} from 'react-native-elements';
 import axios from 'axios';
+import PushController from '../components/PushController';
+import PushNotification from 'react-native-push-notification';
+
 import {bind} from '../utils/utils';
 
 var news;
@@ -17,12 +24,16 @@ export default class News extends Component {
         super(props, context);
         this.state = {
             dataSource: [],
-            isLoading: true
+            isLoading: false,
+            seconds: 5,
         };
-        bind(this)('renderLoadingView', 'goToURL')
+        bind(this)('renderLoadingView', 'goToURL');
+
+        this.handleAppStateChange = this.handleAppStateChange.bind(this);
     }
 
     componentDidMount() {
+
         if (this.state.dataSource.length <= 0) {
             axios.get(`https://api.geonet.org.nz/news/geonet`)
                 .then(res => {
@@ -56,6 +67,22 @@ export default class News extends Component {
             }, 1000 * 60 * 60 * 24);
 
         }
+        AppState.addEventListener('change', this.handleAppStateChange);
+    }
+
+    componentWillUnmount() {
+        AppState.removeEventListener('change', this.handleAppStateChange);
+    }
+
+    handleAppStateChange(appState) {
+        if (appState === 'background') {
+            let date = new Date(Date.now() + (this.state.seconds * 1000));
+
+            PushNotification.localNotificationSchedule({
+                message: "My Notification Message",
+                date: new Date(Date.now() + (5 * 1000)),
+            });
+        }
     }
 
     renderLoadingView() {
@@ -83,6 +110,19 @@ export default class News extends Component {
 
         return (
             <ScrollView>
+
+                <Text style={styles.welcome}>
+                    Choose your notification time in seconds.
+                </Text>
+                <Picker
+                    style={styles.picker}
+                    selectedValue={this.state.seconds}
+                    onValueChange={(seconds) => this.setState({seconds})}
+                >
+                    <Picker.Item label="5" value={5}/>
+                    <Picker.Item label="10" value={10}/>
+                    <Picker.Item label="15" value={15}/>
+                </Picker>
                 <List>
                     {this.state.dataSource.map((news, index) => (
                         <ListItem
@@ -93,8 +133,24 @@ export default class News extends Component {
                         />
                     ))}
                 </List>
+                <Text style={styles.welcome}>
+                    Choose your notification time in seconds.
+                </Text>
+
+                <PushController />
             </ScrollView>
         )
     }
 
 }
+
+const styles = StyleSheet.create({
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    picker: {
+        width: 100,
+    },
+});
