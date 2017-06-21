afab173cef2415c4e70c6b1353881368d824d456
pigflymoon <pplflymoon2015@gmail.com>
2017-06-21 20:38:36 +1200

    :art: Login. Bug: TextInput couldn't be center

--- app/components/Login.js
+++ app/components/Login.js
@@ -7,39 +7,44 @@ import {
     View,
 } from 'react-native';
 
-import { Actions } from 'react-native-router-flux';
+import {Button} from 'react-native-elements';
+import {Actions} from 'react-native-router-flux';
 
 export default class Login extends React.Component {
     state = {
         name: '',
     };
+
     render() {
         return (
             <View style={styles.container}>
                 <Text style={[styles.label, {marginTop: 40}]}>
                     Enter your name :
                 </Text>
-                <TextInput
-                    placeholder='John Smith'
-                    style={styles.textInput}
-                    onChangeText={(text) => {
-                        this.setState({
-                            name: text,
-                        });
-                    }}
-                    value={this.state.name}
-                />
-                <TouchableOpacity
+
+                    <TextInput
+                        placeholder='John Smith'
+                        style={styles.textInput}
+                        onChangeText={(text) => {
+                            this.setState({
+                                name: text,
+                            });
+                        }}
+                        value={this.state.name}
+                    />
+
+
+                <Button
                     onPress={() => {
                         Actions.chat({
                             name: this.state.name,
                         });
                     }}
-                >
-                    <Text style={styles.label}>
-                        Next
-                    </Text>
-                </TouchableOpacity>
+                    backgroundColor="#397af8"
+                    raised
+                    title='Log in'/>
+
+
             </View>
         );
     }
@@ -48,13 +53,19 @@ export default class Login extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        // flexDirection: 'column',
+        justifyContent: 'center',
+
+
     },
     label: {
+        // flex: 1,
         fontSize: 20,
-        marginLeft: 15,
+        alignSelf: 'center',
+
     },
     textInput: {
         height: 40,
-        marginLeft: 15,
+        marginLeft: 50,
     },
 });
